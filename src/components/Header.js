88fb9bdfce5c603c94dd48d6432aeda355d007f9
Hrisikesh <hrisikesh135@gmail.com>
2023-05-03 22:20:45 +0530

    First Commit

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+const Header=()=>{
+   return(
+    <React.Fragment>
+       <header>
+				<h1>
+		              Scribble
+				</h1>
+			</header>
+    </React.Fragment>
+
+   ) 
+}
+
+export default Header;
